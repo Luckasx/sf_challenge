062162ec8871dae58c180b1e505573c8c49c9f27
luckasx <luckasx@gmail.com>
2024-07-16 19:23:49 -0300

    Add Limit and Sort to the DB Query

--- server/dao.py
+++ server/dao.py
@@ -1,7 +1,8 @@
 import os
 
-from pymongo import MongoClient 
+from pymongo import MongoClient , ASCENDING
 
+print("port", os.getenv("MONGO_PORT"))
 client = MongoClient(
                     os.getenv("MONGO_SERVER")
                      , port=int(os.getenv("MONGO_PORT"))
@@ -10,9 +11,9 @@ client = MongoClient(
                      ) 
 
 
-def query(filter):
+def query(filter, limit = 10):
     db = client["sanfrancisco"]    
     col = db["film_locations"]
-    result = col.find(filter,{"Title": 1,"Locations":1,  "_id":0})
+    result = col.find(filter,{"Title": 1,"Locations":1,  "_id":0}, limit=limit).sort('Title', ASCENDING)
     # l = list(result)
     return list(result)
