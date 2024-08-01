bdf7b3da9599281a24f209a0b899e55b2cde4a82
luckasx <luckasx@gmail.com>
2024-08-01 19:25:30 -0300

    Group The BackEnd Query by Title

--- server/dao.py
+++ server/dao.py
@@ -1,21 +1,24 @@
 import os
 
-from pymongo import MongoClient , ASCENDING
+from pymongo import MongoClient, ASCENDING
+
 
 class MAPDao():
     def __init__(self) -> None:
         print("port", os.getenv("MONGO_PORT"))
         self.client = MongoClient(
-                            os.getenv("MONGO_SERVER")
-                            , port=int(os.getenv("MONGO_PORT"))
-                            , username=os.getenv("MONGO_USER")
-                            , password=os.getenv("MONGO_PASS")
-                            ) 
+            os.getenv("MONGO_SERVER"), port=int(os.getenv("MONGO_PORT")), username=os.getenv("MONGO_USER"), password=os.getenv("MONGO_PASS")
+        )
 
-
-    def query(self, filter, limit = 10):
-        db = self.client["sanfrancisco"]    
+    def query(self, filter, limit=10):
+        db = self.client["sanfrancisco"]
         col = db["film_locations"]
-        result = col.find(filter,{"Title": 1,"Locations":1,  "_id":0}, limit=limit).sort('Title', ASCENDING)
-        # l = list(result)
+
+        pipeline = [{"$match": filter},
+                    {"$limit": 10},
+                    { "$sort": { "Title": 1 } },
+                    {"$project": {"Title": 1, "_id": 0}}]
+
+        result = col.aggregate(pipeline)
+
         return list(result)
