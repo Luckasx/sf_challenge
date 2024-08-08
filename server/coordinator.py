35f265e46ccf99f3c3cc84f296f9ed52b4a9456b
luckasx <luckasx@gmail.com>
2024-08-08 19:36:05 -0300

    Implement Location Coordinates Search

--- server/coordinator.py
+++ server/coordinator.py
@@ -1,10 +1,32 @@
-import requests, os, json
+import requests, os, json, time
 
 class Coordinator:
     def __init__(self) -> None:
         self.API_URL = 'https://geocode.maps.co/search?q=address&api_key=apikey'
 
-    def get_coordinates(self, address):
-        resp = requests.get(self.API_URL.replace("apikey", os.getenv("GEOAPI_KEY")).replace('address', address))
-        return json.loads(resp.text)[0]
+    def get_coordinates(self, locations):
+        
+        result = []
+        
+        for location in locations:
+            if "Coordinates" not in location:
+                print(location["Locations"])
+                try:
+                    time.sleep(0.2)
+                    resp = requests.get(self.API_URL.replace("apikey", os.getenv("GEOAPI_KEY")).replace('address', location["Locations"] + " San Francisco"))
+                    data =  json.loads(resp.text)
+                
+                    if len(data) == 0:
+                        continue
+                    
+                    location["Coordinates"] = data[0]["lat"] + "," + data[0]["lon"]                
+                    result.append(location)
+                except Exception as e:
+                    print(e)
+                    print(location)
+                    
+                
+                #TODO  UPDATE MONGO DB WITH COORDINATES
+        
+        return result
     
